e22f113b80d9cde7767a1226e09b8236782b5197
Marc Knaup <marc@knaup.io>
2019-02-15 14:40:56 +0100

    fixed runtime error in server on Windows

--- packages/server/src/modules/paths.ts
+++ packages/server/src/modules/paths.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 
-export const data = path.join(process.env.PWD, 'data/')
+export const data = path.join(process.cwd(), 'data/')
 export const embed = path.join(__dirname, '../../../embed/build/')
 export const configurator = path.join(
   __dirname,
